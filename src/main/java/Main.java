45b132e95a2469033c4a887250c3e6bb8b365f07
Smile_NS <moyohi498@nezumi.be>
2023-02-27 00:19:37 +0900

    add: organization to make jumbo-texture files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,15 +21,59 @@ public class Main {
     ));
 
     public static void main(String[] args) throws IOException {
+        //generateMonoJson();
         SimpleJson json = new SimpleJson(new File("./output.json"));
         Map<String, Object> map = json.toMap();
 
-        List<Map.Entry<String, Object>> list = new ArrayList<>(map.entrySet());
-        list.sort(Comparator.comparing((Map.Entry<String, Object> obj) -> ((Integer) obj.getValue())));
+        File[] models = new File("./models/").listFiles();
 
+        assert models != null;
+        for (File md : models) {
+            SimpleJson mdJson = new SimpleJson(md);
+            if (mdJson.containsNode("parent")) {
+                String parent = mdJson.getString("parent");
+                if (!parentList.contains(parent)) continue;
+                if (!parent.equals("minecraft:block/cube_all")) continue;
 
-        for (Map.Entry<String, Object> e : list) {
-            System.out.println(e.getKey() + ": " + e.getValue());
+                //List<BufferedImage> textures = new ArrayList<>();
+                String path = "textures.all";
+                if (mdJson.containsNode(path)) {
+                    String basename = mdJson.getString(path);
+                    if (!basename.contains("minecraft:block/")) continue;
+                    String tx = basename.substring(basename.lastIndexOf('/'));
+                    BufferedImage bi = ImageIO.read(new File("images/" + tx + ".png"));
+                    SimpleJson jumboJson = new SimpleJson(new File("jumbo_textures/" + tx + ".json"));
+
+                    for (int i = 0;i < bi.getWidth();i++) {
+                        for (int j = 0;j < bi.getHeight();j++) {
+                            Color c1 = new Color(bi.getRGB(i, j));
+                            int r1 = c1.getRed();
+                            int g1 = c1.getGreen();
+                            int b1 = c1.getBlue();
+
+                            int minDis = 195075;
+                            String pxlBlock = "";
+                            for (Map.Entry<String, Object> e : map.entrySet()) {
+                                Color c2 = new Color((Integer) e.getValue());
+                                int r2 = c2.getRed();
+                                int g2 = c2.getGreen();
+                                int b2 = c2.getBlue();
+
+                                int dis = (int) (Math.pow(r2 - r1, 2) + Math.pow(g2 - g1, 2) + Math.pow(b2 - b1, 2));
+                                if (dis < minDis) {
+                                    minDis = dis;
+                                    pxlBlock = e.getKey();
+                                }
+                            }
+
+                            System.out.println(minDis);
+                            jumboJson.put("(" + i + ", " + j + ")", pxlBlock);
+                        }
+                    }
+
+                    jumboJson.save();
+                }
+            }
         }
     }
 
@@ -57,35 +101,43 @@ public class Main {
                     if (mdJson.containsNode(path)) {
                         String basename = mdJson.getString(path);
                         if (!basename.contains("minecraft:block/")) continue;
-                        String block = basename.substring(basename.lastIndexOf('/'));
-                        BufferedImage bi = ImageIO.read(new File("images/" + block + ".png"));
+                        String tx = basename.substring(basename.lastIndexOf('/'));
+                        BufferedImage bi = ImageIO.read(new File("images/" + tx + ".png"));
                         textures.add(bi);
                     }
                 }
 
                 if (textures.size() == 0) continue;
 
-                int sum1 = 0;
+                int r1 = 0;
+                int g1 = 0;
+                int b1 = 0;
                 for (BufferedImage bi : textures) {
-                    int sum2 = 0;
+                    int r2 = 0;
+                    int g2 = 0;
+                    int b2 = 0;
                     for (int i = 0;i < bi.getWidth();i++) {
                         for (int j = 0;j < bi.getHeight();j++) {
-                            int c = bi.getRGB(i, j);
-                            Color color = new Color(c);
-                            if (color.getAlpha() != 0) {
-                                sum2 += c;
+                            Color c = new Color(bi.getRGB(i, j));
+                            if (c.getAlpha() != 0) {
+                                r2 += c.getRed();
+                                g2 += c.getGreen();
+                                b2 += c.getBlue();
                             }
                         }
                     }
 
-                    sum1 += (sum2 / (bi.getWidth() * bi.getHeight()));
+                    int pixel = bi.getWidth() * bi.getHeight();
+                    r1 += (r2 / pixel);
+                    g1 += (g2 / pixel);
+                    b1 += (b2 / pixel);
                 }
 
-                int mono = sum1 / textures.size();
+                Color mono = new Color(r1 / textures.size(), g1 / textures.size(), b1 / textures.size());
                 String filename = md.getName();
                 String name = filename.substring(0, filename.lastIndexOf('.'));
-                System.out.println(name + ": " + mono);
-                json.put(name, mono);
+                System.out.println(name + ": " + mono.getRGB());
+                json.put(name, mono.getRGB());
             }
         }
 
